a8d919af42f169a68dd6cb3ec8b43657a7bd3865
Zenon Zeniou <zzeniou86@gmail.com>
2017-02-13 18:04:19 +0000

    cache implementation: add expiry and a helpful comment
    Signed-off-by: Zenon Zeniou <zzeniou86@gmail.com>

--- models/disruptions_cache.rb
+++ models/disruptions_cache.rb
@@ -9,8 +9,11 @@ class DisruptionsCache
     if doc_etag != cached_etag 
         coords = yield
         $redis.set "coords", coords.to_json
+        # Comment the following two lines of code if we don't need to store historical data 
         $redis.set "etag:#{doc_etag}", coords.to_json
+        $redis.expire "etag:#{doc_etag}", 86400
         $redis.set "etag", doc_etag
+        $redis.expire "etag", 600
     end      
   end
 
